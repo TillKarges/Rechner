a8a4d5519a3b638b3b0b60f908f1bba9b397c3a2
TillKarges <till-karges@gmx.de>
2015-05-21 13:52:30 +0200

    Update Aufgabe2.java

--- Aufgabe2.java
+++ Aufgabe2.java
@@ -1,1 +1,59 @@
+import java.util.Scanner;
+
+public class Aufgabe2 {
+
+	static Scanner s;
+
+	public static void main(String args[]){
+		
+		s = new Scanner(System.in);
+		
+		int keepRunning = 1;
+		
+		while (keepRunning == 1){
+			System.out.println("Hallo ich bin Programm 700.0");
+			
+			System.out.println("Gib die erste Zahl ein.");
+				int a = s.nextInt();
+			
+			System.out.println("Gib den Operator ein.");
+				String operator = s.next();
+			
+			System.out.println("Gib die zweite Zahl ein.");
+				int b = s.nextInt();
+			
+			double result = 0;
+			
+			switch (operator){
+				
+				case "+":
+					result = a + b;
+					break;
+					
+				case "-":
+					result = a - b;
+					break;
+					
+				case "*":
+					 result = a * b;
+					break;
+					
+				case "/":
+					result = a / b;
+					break;
+					
+				default:
+					System.out.println("Diesen Operator " + operator + "kenne ich nicht!");
+					result = 0;
+			}
+			String ausgabe = "Ergebnis von " + a + " " + operator + " " + b + " = " + result;
+			
+			System.out.println(ausgabe);
+			
+			System.out.println("Zum weiter rechnen 1 drücken.");
+			keepRunning = s.nextInt();
+			
+		}
+	}
+}
 
